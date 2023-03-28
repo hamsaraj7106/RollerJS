bcffc2aa73ab95e30a18a83317f8cd956c524fb7
hamsaraj <hamsaraj.tupthi18@gmail.com>
2023-03-28 10:20:01 -0700

    Added functionality and unit tests to Roller class

--- src/Roller.test.ts
+++ src/Roller.test.ts
@@ -10,3 +10,72 @@ describe("Roller tests", ()=> {
   test("Description", () => {
   });
 });
+
+describe("Roller", () => {
+  describe("constructor", () => {
+    test("should create a six-sided die if no faces are provided", () => {
+      const roller = new Roller(0);
+      expect(roller["_faces"]).toBe(6);
+    });
+
+    test("should create a die with the specified number of faces", () => {
+      const roller = new Roller(10);
+      expect(roller["_faces"]).toBe(10);
+    });
+
+    test("should default to a six-sided die if an invalid number of faces is provided", () => {
+      const roller = new Roller(-1);
+      expect(roller["_faces"]).toBe(6);
+    });
+  });
+
+  describe("roll", () => {
+    test("should record a valid roll and update the distribution", () => {
+      const roller = new Roller(4);
+      expect(roller.roll(2)).toBe(2);
+      expect(roller["_last"]).toBe(2);
+      expect(roller.distribution().get(2)).toBe(1);
+    });
+
+    test("should not record an invalid roll and return 0", () => {
+      const roller = new Roller(4);
+      expect(roller.roll(5)).toBe(0);
+      expect(roller["_last"]).toBe(0);
+      expect(roller.distribution().get(5)).toBe(undefined);
+    });
+  });
+
+  describe("last", () => {
+    test("should return the last roll", () => {
+      const roller = new Roller(6);
+      expect(roller.roll(3)).toBe(3);
+      expect(roller.last()).toBe(3);
+    });
+
+    test("should return 0 if no rolls have been made", () => {
+      const roller = new Roller(6);
+      expect(roller.last()).toBe(0);
+    });
+  });
+
+  describe("distribution", () => {
+    test("should return the distribution of rolls", () => {
+      const roller = new Roller(3);
+      roller.roll(1);
+      roller.roll(2);
+      roller.roll(3);
+      roller.roll(2);
+      expect(roller.distribution().get(1)).toBe(1);
+      expect(roller.distribution().get(2)).toBe(2);
+      expect(roller.distribution().get(3)).toBe(1);
+    });
+
+    test("should return a Map with keys for all possible faces", () => {
+      const roller = new Roller(4);
+      expect(roller.distribution().get(1)).toBe(undefined);
+      expect(roller.distribution().get(2)).toBe(undefined);
+      expect(roller.distribution().get(3)).toBe(undefined);
+      expect(roller.distribution().get(4)).toBe(undefined);
+    });
+  });
+});
--- src/Roller.ts
+++ src/Roller.ts
@@ -1,41 +1,55 @@
 export class Roller {
-    private _distribution: Map<number, number>;
-    private _last: number;
-    private _faces: number;
+  private _distribution: Map<number, number> = new Map<number, number>();
+  private _last: number = 0;
+  private _faces: number;
 
-    // specifies the number of faces the die has. It must have at least 2 faces
-    // so any invalid values should default to 6 to represent the common 
-    // six-sided die.
-    constructor(faces: number) {
-        faces;
+  // specifies the number of faces the die has. It must have at least 2 faces
+  // so any invalid values should default to 6 to represent the common
+  // six-sided die.
+  constructor(faces: number) {
+    if (faces >= 2) {
+      this._faces = faces;
+    } else {
+      this._faces = 6;
     }
+  }
 
-    // specifies which number should be rolled and returns the recorded roll. 
-    // If the value provided is not valid for the number of faces the die has,
-    // no record of the roll should be made and 0 should be returned. However,
-    // if the value provided is between 1 and the number of faces (inclusive),
-    // then:
-    //   1. that same value should be returned,
-    //   2. that value should be stored as the last roll, and
-    //   3. the distribution should update the distribution record so that the
-    //      key (value rolled) has an updated value that increases the number
-    //      of rolls for that face by 1
-    roll(value: number): number{
-        return value;
+  // specifies which number should be rolled and returns the recorded roll.
+  // If the value provided is not valid for the number of faces the die has,
+  // no record of the roll should be made and 0 should be returned. However,
+  // if the value provided is between 1 and the number of faces (inclusive),
+  // then:
+  //   1. that same value should be returned,
+  //   2. that value should be stored as the last roll, and
+  //   3. the distribution should update the distribution record so that the
+  //      key (value rolled) has an updated value that increases the number
+  //      of rolls for that face by 1
+  roll(value: number): number {
+    if (value >= 1 && value <= this._faces) {
+      this._last = value;
+      if (this._distribution.has(value)) {
+        this._distribution.set(value, this._distribution.get(value) + 1);
+      } else {
+        this._distribution.set(value, 1);
+      }
+      return value;
+    } else {
+      return 0;
     }
+  }
 
-    // returns the value of the latest die roll. If no rolls have been made
-    // yet, then it should return 0.
-    last(): number {
-        return 0;
-    }
+  // returns the value of the latest die roll. If no rolls have been made
+  // yet, then it should return 0.
+  last(): number {
+    return this._last;
+  }
 
-    // returns a Map that represents the current distribution of rolls for each
-    // possible face that can be rolled. The map's key should be the face
-    // value and its value should be the number of times that face has been
-    // rolled. Even if a face has not yet been rolled, its key should have a
-    // value of 0.
-    distribution(): Map<number, number>{
-        return new Map<number, number>();
-    }
-} // end of Roller class
+  // returns a Map that represents the current distribution of rolls for each
+  // possible face that can be rolled. The map's key should be the face
+  // value and its value should be the number of times that face has been
+  // rolled. Even if a face has not yet been rolled, its key should have a
+  // value of 0.
+  distribution(): Map<number, number> {
+    return this._distribution;
+  }
+}
